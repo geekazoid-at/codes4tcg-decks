40a77844a66c75b42aebfe20f2572c8f007ca2a5
habu <bretschneider.andreas@gmail.com>
2025-03-12 17:30:39 +0100

    headless puppeteer

--- previewBot/src/screenshot.js
+++ previewBot/src/screenshot.js
@@ -1,7 +1,19 @@
 import puppeteer from "puppeteer";
 
 export const takeScreenshot = async (url, outputPath) => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process",
+      "--disable-gpu",
+    ],
+  });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle2" });
   await page.setViewport({ width: 1146, height: 644 });
